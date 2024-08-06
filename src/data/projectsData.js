7b7cb8659ae9545fdbddf6e6761a625c5a6d21bb
SARAN S M <saran007mgp@gmail.com>
2024-08-06 13:48:32 +0530

    Github link updated

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -14,8 +14,8 @@ export const projectsData = [
         projectName: 'Event Management System (EMS)',
         projectDesc: 'A MERN stack application for managing events. Users can view, book events, download tickets, get them scanned for attendance, and download certificates.',
         tags: ['React', 'Express', 'MongoDB'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://github.com/SARAN-S-M/EMS',
+        demo: 'https://ems-two-orcin.vercel.app/',
         image: one
     },
     {
@@ -23,8 +23,8 @@ export const projectsData = [
         projectName: 'Compiler Project',
         projectDesc: 'An interactive compiler system where students can compile and test their code, attend tests, and view results. Staff can create classrooms, assign tests, and manage student progress. Built with MERN stack and MySQL for data management.',
         tags: ['React', 'Express', 'MySQL'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://github.com/RajendranDinesh/fortis',
+        demo: 'https://consus.vercel.app/',
         image: two
     },
     {
@@ -32,8 +32,8 @@ export const projectsData = [
         projectName: 'Invoice Portal',
         projectDesc: 'A MERN stack app for managing college resources, allowing booking of faculty interactions and equipment, with usage insights.',
         tags: ['React', 'Express', 'MongoDB'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://github.com/SARAN-S-M/invoice_portal',
+        demo: 'https://github.com/SARAN-S-M/invoice_portal',
         image: three
     },
     {
@@ -41,8 +41,8 @@ export const projectsData = [
         projectName: 'Special Lab Portal',
         projectDesc: 'A MERN stack app for students to book slots for special labs, manage activities through a dashboard, and access additional features for enhanced user experience.',
         tags: ['React', 'Express', 'MySQL'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://github.com/SARAN-S-M/special_lab_portal',
+        demo: 'https://github.com/SARAN-S-M/special_lab_portal',
         image: four
     },
     {
@@ -50,8 +50,8 @@ export const projectsData = [
         projectName: 'Amazon Clone',
         projectDesc: 'A single-page Amazon clone showcasing my CSS skills. This project highlights my ability to create visually appealing layouts using just HTML and CSS. An impressive display of front-end design capabilities!',
         tags: ['HTML', 'CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://github.com/SARAN-S-M/amazon-clone',
+        demo: 'https://github.com/SARAN-S-M/amazon-clone',
         image: five
     },
     // {
